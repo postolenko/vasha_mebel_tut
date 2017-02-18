ead48406422b9b63a9a3318201035ed9f79e01f5
postolenko <denis.postolenko@yandex.ua>
2017-02-18 07:36:10 +0200

    Vasha Mebel Tut

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    //   preload
+    // $(window).on('load', function () {
+
+    //     setTimeout(function() {
+
+    //         $(".preload-bg").fadeOut(500);
+
+    //     }, 700);
+  
+
+    // });
+
+
+    $(window).resize(function() {
+
+
+        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+        $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    });
+
+
+    $(function() {
+
+        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+        $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    });
+
+});
