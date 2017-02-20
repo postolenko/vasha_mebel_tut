a3daf1b717bb4a2550c5ef2f3a3822c6c11b5357
postolenko <denis.postolenko@yandex.ua>
2017-02-20 18:39:47 +0200

    Vasha Mebel Tut

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # http://postolenko.github.io/vasha_mebel_tut/index.html
 # http://postolenko.github.io/vasha_mebel_tut/catalog.html
 # http://postolenko.github.io/vasha_mebel_tut/gallery.html
+# http://postolenko.github.io/vasha_mebel_tut/product_page.html
--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,9 @@ $(document).ready(function() {
 
     // });
 
+    var popupImgWidth;
+    var popupImgHeight;
+
 
     $(window).resize(function() {
 
@@ -25,6 +28,8 @@ $(document).ready(function() {
         $(".gallery-box").css({"top" : ( $(window).height() - $(".gallery-box").outerHeight(true) ) / 2 + "px" });
 
         // ---------------------------------------------------------------------------------
+
+        getSlideImgSize();
 
     });
 
@@ -45,6 +50,35 @@ $(document).ready(function() {
 
     $(function() {
 
+        var indexProductSlider;
+        var srcActiveImg;
+
+        $(".show-big-photo").click(function() {
+
+            indexProductSlider = $(this).index(".show-big-photo");
+
+            srcActiveImg = $(".big_photo_slider:eq("+ indexProductSlider  +") li:visible img").attr("src");
+
+            $(".product-slide-popup").fadeIn(300);
+
+            $(".product-slide-popup img").attr("src", srcActiveImg);
+
+            getSlideImgSize();            
+
+        });
+
+        $(".close-product-popup, .popup-bg").click(function() {
+
+            $(".product-slide-popup").fadeOut(300);
+
+        });
+
+    });
+
+    // --------------------------------------------------------------------------------------
+
+    $(function() {
+
         $(".gallery-bg, .close-gallery").click(function() {
 
             $(".gallery-sect").fadeOut(300);
@@ -52,5 +86,92 @@ $(document).ready(function() {
         });
 
     });
+
+    // --------------------------------------------------------------------------------------
+
+    $(function() {
+
+        $(".img-material-box").click(function() {
+
+            $(this).prev("input").click();
+
+        });
+
+    });
+
+    // --------------------------------------------------------------------------------------
+
+    $(function() {
+
+        var materialsIndex;
+
+        var materialsHeight;
+
+        $(".material-hide").click( function() {
+
+            materialsIndex = $(".material-hide").index(this);
+
+            materialsHeight = $(".materials:eq("+ materialsIndex +")").outerHeight(true);
+
+            if( $(".materials-wrapp:eq("+ materialsIndex +")").height() <= 0 ) {
+
+                $(this).removeClass("hide");
+
+                $(".materials-wrapp:eq("+ materialsIndex +")").removeClass("hide");
+
+                $(".materials-wrapp:eq("+ materialsIndex +")").animate({"height" : materialsHeight  + "px"}, 300);
+
+            } else {
+
+                $(this).addClass("hide");
+
+                $(".materials-wrapp:eq("+ materialsIndex +")").addClass("hide");
+
+                $(".materials-wrapp:eq("+ materialsIndex +")").animate({"height" : 0 + "px"}, 300);
+
+            }
+
+        });
+
+    });
+
+    // --------------------------------------------------------------------------------------
+
+    function getSlideImgSize() {
+
+        if( $(".popup-img").is(":visible") ) {
+
+            popupImgWidth = $(".popup-img").width();
+
+            popupImgHeight = $(".popup-img").height();
+
+            if( popupImgWidth > $(window).width() * .9 || popupImgHeight > $(window).height() * .9 ) {
+
+                $(".popup-img").css({
+
+                    "max-width" : 90 + "%",
+                    "max-height" : 90 + "%",
+                    "min-width": "auto",
+                    "min-height": "auto"
+
+                });
+
+            }
+            // else if ( popupImgWidth < $(window).width() * .9 || popupImgHeight < $(window).height() * .9 ) {
+
+            //     $(".popup-img").css({
+
+            //         "max-width" : "auto",
+            //         "max-height" : "auto",
+            //         "min-width": 70 + "%",
+            //         "min-height": 70 + "%"
+
+            //     });
+
+            // }
+
+        }
+
+    }
 
 });
